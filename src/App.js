2f3d4e2e0764d0e53827e0afdaa4fefdd9234394
Andrew Emil Bizon <h.c.draconis@Andrew-macbook.local>
2020-05-20 06:49:44 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let index = 20;
+
+function App() {
+  const [count, setCount] = useState(10);
+  // let count = 10;
+  function myOnClickCount(e) {
+    // count++;
+    console.log('we just clicked', count);
+    setCount(count + 1);
+  
+  }
+  function myOnClickIndex(e) {
+    index++;
+    console.log('we just clicked', index);
+  
+  }
+  return (
+    <div className="App">
+      <div>
+  <h1 onClick={myOnClickCount}>Hello World: count {count}</h1>
+  <h1 onClick={myOnClickIndex}>Hello World: index {index}</h1>
+      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
